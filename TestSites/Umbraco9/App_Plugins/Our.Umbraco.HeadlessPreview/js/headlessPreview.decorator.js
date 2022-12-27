ef03c08902818786c3f3603a9348a6cd775b9e9c
Jesper Weber <jweber@jweber.dk>
2022-12-27 17:27:43 +0100

    Added test sites

---
+++ TestSites/Umbraco9/App_Plugins/Our.Umbraco.HeadlessPreview/js/headlessPreview.decorator.js
@@ -0,0 +1,25 @@
+﻿var contentLoadedName = 'headlessPreview.content.loaded';
+angular.module(contentLoadedName, []).config(['$provide', $provide => {
+    $provide.decorator("$rootScope", function preview($delegate, previewService) {
+        $delegate.$on('content.loaded', (_, data) => {
+
+            previewService.changePreviewButton(data);
+
+        });
+        return $delegate;
+    });
+}]);
+angular.module('umbraco').requires.push(contentLoadedName);
+
+var contentSavedName = 'headlessPreview.content.saved';
+angular.module(contentSavedName, []).config(['$provide', $provide => {
+    $provide.decorator("$rootScope", function preview($delegate, previewService) {
+        $delegate.$on('content.saved', (_, data) => {
+
+            previewService.changePreviewButton(data);
+
+        });
+        return $delegate;
+    });
+}]);
+angular.module('umbraco').requires.push(contentSavedName);
